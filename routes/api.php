71bb102815ea32a4f3a267dee20198a2c7e7c93b
kkoouuya0730 <kouya.0730@icloud.com>
2025-10-25 16:39:44 +0900

    sanctumで認証機能実装

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,20 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 Route::apiResource('posts', PostController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('posts', PostController::class)->except(['index', 'show']);
+});
 
 Route::get('/test', function () {
     return response()->json(['message' => 'API OK']);
+});
+
+use App\Http\Controllers\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
